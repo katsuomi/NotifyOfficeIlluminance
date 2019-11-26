500ace7ae56849254160890f948511afa3995783
katsuomi <mugichak1227@gmail.com>
2019-11-26 19:06:14 +0900

    require外す

--- models/illuminance.go
+++ models/illuminance.go
@@ -6,7 +6,7 @@ import (
 
 // Illuminance is Illuminance models property
 type Illuminance struct {
-	ID    			 uint      `json:"id" binding:"required"`
-	Illuminance  int       `json:"illuminance" binding:"required"`	
-	CreatedAt    time.Time `json:"created_at"`
+	ID          uint      `json:"id"`
+	Illuminance int       `json:"illuminance" binding:"required"`
+	CreatedAt   time.Time `json:"created_at"`
 }
